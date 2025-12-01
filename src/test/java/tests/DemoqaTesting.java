508dc8c0bb3333272dff3ddcf069bf985eeb4210
t5582871-svg <dsvredlov@mail.ru>
2025-12-02 00:41:24 +0300

    Added tests DemogaTest.java

---
+++ src/test/java/tests/DemoqaTesting.java
@@ -0,0 +1,64 @@
+package tests;
+
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
+
+public class DemoqaTesting {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Timur");
+        $("#lastName").setValue("Dasaev");
+
+        $("#userEmail").setValue("email@example.ru");
+
+        $("#genterWrapper .custom-control-label").click();
+
+        $("#userNumber").setValue("8800555353");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText("January")).click();
+        $(".react-datepicker__year-select").$(byText("1997")).click();
+        $(".react-datepicker__month").$(byText("8")).click();
+
+        $("#subjectsContainer input").setValue("English").pressEnter();
+
+        $("#hobbiesWrapper .custom-control-label").click();
+
+        $("#uploadPicture").uploadFromClasspath("dog.png");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        $("#currentAddress").setValue("Shicago");
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street 1"));
+        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+    }
+}
