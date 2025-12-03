045fcb4c5216172e6f4a05392e138d7063c91d28
t5582871-svg <dsvredlov@mail.ru>
2025-12-03 20:22:09 +0300

    Added shouldHave(text())

--- src/test/java/tests/DemoqaTest.java
+++ src/test/java/tests/DemoqaTest.java
@@ -2,27 +2,22 @@ package tests;
 
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
 
-public class DemoqaTesting {
+
+public class DemoqaTest {
 
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
 
@@ -31,34 +26,32 @@ public class DemoqaTesting {
         open("/automation-practice-form");
         $("#firstName").setValue("Timur");
         $("#lastName").setValue("Dasaev");
-
         $("#userEmail").setValue("email@example.ru");
-
         $("#genterWrapper .custom-control-label").click();
-
         $("#userNumber").setValue("8800555353");
-
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").$(byText("January")).click();
         $(".react-datepicker__year-select").$(byText("1997")).click();
         $(".react-datepicker__month").$(byText("8")).click();
-
         $("#subjectsContainer input").setValue("English").pressEnter();
-
         $("#hobbiesWrapper .custom-control-label").click();
-
-        $("#uploadPicture").uploadFromClasspath("dog.png");
-
+        $("#uploadPicture").uploadFromClasspath("artworks.jpg");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
-
         $("#currentAddress").setValue("Shicago");
+        $("#submit").click();
 
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@egorov.com"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
-        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        $(".table-responsive").shouldHave(text("Timur Dasaev"));
+        $(".table-responsive").shouldHave(text("email@example.ru"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("8800555353"));
+        $(".table-responsive").shouldHave(text("08 January,1997"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("Sports"));
+        $(".table-responsive").shouldHave(text("artworks.jpg"));
+        $(".table-responsive").shouldHave(text("Shicago"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
     }
 }
